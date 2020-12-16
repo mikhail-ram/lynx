be252be28104d6bc64d0035a041a8376d2978449
Mikhail <mikhailethanram@gmail.com>
2020-12-16 23:51:41 +0530

    Cleaned up code. Checkpoint commit.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::env;
 
 enum Token {
     NEWLINE,
@@ -30,7 +31,8 @@ enum Token {
 }
 
 fn main() {
-    let contents = fs::read_to_string("../Hello World.lynx")
+    let args: Vec<String> = env::args().collect();
+    let contents = fs::read_to_string(&args[1])
                    .expect("Something went wrong reading the file");
 
     compile(contents);
@@ -38,76 +40,66 @@ fn main() {
 
 fn compile(program: String) {
     let tokens = lexer(program);
-    // println!("{:?}", tokens);
+    // println!("{:#?}", tokens);
 }
 
 fn lexer(program: String) {
+    let mut tokens: Vec<Token> = Vec::new();
     for line in program.lines() {
-        if line == "" { continue }
-        let mut word = String::new();
+        if line == "" { continue; }
         let mut string_started = false;
         let mut goes_into = false;
+        let mut word = String::new();
         let mut gets_from = false;
         let mut iterator = line.chars().peekable();
         while let Some(char) = iterator.next() {
-        // for char in line.chars() {
-            if char.is_alphanumeric() {
-                word.push(char);
-            }
-            else if !word.is_empty() {
-                println!("{:?}", word);
-                word.clear();
+            let peek = *iterator.peek().unwrap_or(&' ');
+            {
+                if char.is_alphanumeric() {
+                    word.push(char);
+                }
+                else if !word.is_empty() {
+                    if word.chars().all(char::is_alphabetic) {
+                        println!("IDENT({})", word);
+                    }
+                    else {
+                        println!("NUMBER({})", word);
+                    }
+                    word.clear();
+                }
             }
             if char == '"' {
                 let mut string_started = !string_started;
             }
-            if char == '=' {
+            else if char == '=' {
                 println!("EQUALS");
             }
             else if char == '/' && !char.is_alphanumeric() {
-                let peek = *iterator.peek().expect("Program failed. Can't tell what '/' refers to.");
                 if peek == '/' {
                     break;
                 }
-                else {
-                    println!("DIVIDE");
-                }
+                else { println!("DIVIDE") }
             }
-            else if char == '>' {
-                println!("GREATERTHAN");
-            }
+            else if char == '>' { println!("GREATERTHAN") }
             else if char == '<' {
-                let peek = *iterator.peek().expect("Program failed. Can't tell what '<' refers to.");
                 if peek == '(' || peek == '-' {
                     println!("GETSFROM");
                     gets_from = true;
                     iterator.next();
                 }
-                else {
-                    println!("LESSTHAN");
-                }
+                else { println!("LESSTHAN") }
             }
             else if char == ':' {
-                let peek = *iterator.peek().expect("Program failed. Can't tell what ':' refers to.");
                 if peek == ':' {
                     println!("BLOCK");
                     break;
                 }
             }
-            else if char == ',' {
-                println!("COMMA");
-            }
-            else if char == '[' {
-                println!("LSQUAREBRACKET");
-            }
-            else if char == ']' {
-                println!("RSQUAREBRACKET");
-            }
-            else if char == '(' {
-                println!("LBRACKET");
-            }
+            else if char == ',' { println!("COMMA") }
+            else if char == '[' { println!("LSQUAREBRACKET") }
+            else if char == ']' { println!("RSQUAREBRACKET") }
+            else if char == '(' { println!("LBRACKET") }
             else if char == ')' {
-                let peek = *iterator.peek().unwrap_or(&' ');
                 if peek == '>' && goes_into {
                     println!("GOESINTO");
                     goes_into = false;
@@ -118,32 +110,26 @@ fn lexer(program: String) {
                     gets_from = false;
                     iterator.next();
                 }
-                else {
-                    println!("RBRACKET");
-                }
+                else { println!("RBRACKET") }
             }
             else if char == '-' {
-                let peek = *iterator.peek().expect("Program failed. Can't tell what '-' refers to.");
                 if peek == '>' {
                     println!("GOESINTO");
                     iterator.next();
                 }
                 else if peek == '(' {
                     iterator.next();
-                    let peek = *iterator.peek().expect("Program failed. Can't tell what '(' refers to.");
-                    if peek.is_alphabetic() {
+                    let next_peek = *iterator.peek().expect("Program failed. Can't tell what '(' refers to.");
+                    if next_peek.is_alphabetic() {
                         println!("GOESINTO");
                         goes_into = true;
                     }
-                    else {
-                        println!("MINUS");
-                    }
+                    else { println!("MINUS") }
                 }
-                else {
-                    println!("MINUS");
-                }
+                else { println!("MINUS") }
             }
         }
-        println!("{:?}", word);
+        if word != "" { println!("{}", word) }
+        println!("NEWLINE");
     }
 }
